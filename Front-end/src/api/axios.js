5fac28367cac5bddf1384a3ebdca36eb794ee37e
kale mahesh <kalemahesh082003@gmail.com>
2026-06-04 17:52:47 +0530

    Fix production API URL

--- Front-end/src/api/axios.js
+++ Front-end/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? '/api' : 'http://localhost:5001/api'),
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Interceptor to add JWT token to requests
@@ -13,9 +13,31 @@ api.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
+
+
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? '/api' : 'https://resumescore.vercel.app/api'),
+// });
+
+// // Interceptor to add JWT token to requests
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
+
